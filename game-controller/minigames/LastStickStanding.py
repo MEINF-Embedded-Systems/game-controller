2f8637989b5b70bba321e40bfbd3a08da60b7173
Eric López <lopezs.eric@gmail.com>
2025-01-07 18:26:12 +0100

    LastStickStanding

---
+++ game-controller/minigames/LastStickStanding.py
@@ -0,0 +1,93 @@
+import json
+import time
+import paho.mqtt.client as mqtt
+from random import Random
+from threading import Event
+from minigames import Minigame
+from Player import Player
+from Utils import Utils, LCDMessage
+
+BUTTON_TOPIC = "game/players/{id}/components/button"
+
+class LastStickStanding(Minigame):
+    """
+    Last Stick Standing:
+    Players take turns removing sticks from a pile. The player who removes the last stick loses.
+    """
+
+    def __init__(self, players: list[Player], client: mqtt.Client) -> None:
+        super().__init__(players, client)
+        self.sticks = 9
+        self.utils = Utils(client, players, debug=True)
+        self.lastStickStandingEvent = Event()
+        self.current_player_index = 0
+        self.sticks_to_take = 1
+
+    def playGame(self) -> list[Player]:
+        self.utils.printDebug(f"Starting game with {self.sticks} sticks")
+        self.introduceGame()
+        self.client.subscribe(BUTTON_TOPIC.format(id="+"))
+        self.showTurnInfo()
+        self.lastStickStandingEvent.wait()
+        self.client.unsubscribe(BUTTON_TOPIC.format(id="+"))
+        time.sleep(2)
+        winners = [player for player in self.players if player.id != self.last_player]
+        return winners  # Return winners directly without additional filtering
+    
+    def handleMQTTMessage(self, message: mqtt.MQTTMessage) -> None:
+        player_id = int(message.topic.split("/")[2])
+        if message.topic == BUTTON_TOPIC.format(id=player_id):
+            payload = json.loads(message.payload.decode("utf-8"))
+            press_type = payload["type"]
+            self.utils.printDebug(payload)
+            if press_type == "short":
+                self.toggleSticksToTake()
+            elif press_type == "long":
+                self.removeStick(player_id)
+
+    def showTurnInfo(self) -> None:
+        current_player = self.players[self.current_player_index]
+        self.utils.printDebug(f"Turn: Player {current_player.id} - Sticks remaining: {self.sticks}")
+        # Show turn info to current player
+        self.utils.showInLCD(current_player.id, LCDMessage(top="Your turn!", down=f"Take: {self.sticks_to_take} sticks"))
+        # Show wait info to other players
+        self.utils.showInOtherLCD(current_player.id, LCDMessage(top=f"Player {current_player.id}'s turn", down=f"Sticks left: {self.sticks}"))
+
+    def showRemainingSticks(self) -> None:
+        sticks_visual = "| " * self.sticks
+        self.utils.showInAllLCD(
+            LCDMessage(top=f"Sticks taken: {self.sticks_to_take}", down=sticks_visual.strip()))
+
+    def toggleSticksToTake(self) -> None:
+        if self.sticks >= 2:
+            self.sticks_to_take = 2 if self.sticks_to_take == 1 else 1
+            self.utils.printDebug(f"Player {self.players[self.current_player_index].id} selected to take {self.sticks_to_take} sticks")
+        else:
+            self.sticks_to_take = 1
+            self.utils.printDebug(f"Only 1 stick can be taken")
+        self.showTurnInfo()
+
+    def removeStick(self, player_id: int) -> None:
+        if player_id == self.players[self.current_player_index].id:
+            self.utils.printDebug(f"Player {player_id} removes {self.sticks_to_take} sticks")
+            self.sticks -= self.sticks_to_take
+            self.showRemainingSticks()
+            time.sleep(2)
+            
+            if self.sticks <= 1:  # Changed from <= 0
+                self.last_player = player_id
+                self.utils.printDebug(f"Game Over - Player {player_id} loses!")
+                self.utils.showInLCD(
+                    player_id,
+                    LCDMessage(top="Game Over!", down="You lose!")
+                )
+                self.utils.showInOtherLCD(
+                    player_id,
+                    LCDMessage(top="Game Over!", down="You win!")
+                )
+                time.sleep(2)
+                self.lastStickStandingEvent.set()
+            else:
+                self.current_player_index = (self.current_player_index + 1) % len(self.players)
+                self.sticks_to_take = 1
+                self.showTurnInfo()
