fa9c961f23f73667fbdc4952fde8a8a6ac02b3f4
Mihaela <mbuturuga@gmail.com>
2025-01-09 16:53:52 +0100

    create game tune

--- game-controller/Melodies.py
+++ game-controller/Melodies.py
@@ -1,6 +1,9 @@
 from Message import BuzzerMessage
 
-GAME_TUNE = None
+GAME_TUNE = BuzzerMessage(
+    tones=[262, 330, 392, 523, 392, 330, 262, 392, 0],
+    duration=[150, 150, 150, 300, 150, 150, 150, 300],
+)
 
 WINNING_SOUND = BuzzerMessage(tones=[262, 294, 330, 349, 392, 0], duration=[150] * 5)
 
